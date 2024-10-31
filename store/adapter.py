3fd64efa9a8291a102fa6e007d0d6847ce4e5cbf
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2024-10-31 23:42:40 +0700

    fix login oath

--- store/adapter.py
+++ store/adapter.py
@@ -1,6 +1,9 @@
 from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from store.models import User
+from django.shortcuts import redirect
+from django.contrib import messages
+from allauth.core.exceptions import ImmediateHttpResponse
 
 class NoSignupAccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request):
@@ -11,19 +14,17 @@ class NoSignupSocialAccountAdapter(DefaultSocialAccountAdapter):
         return True
 
     def pre_social_login(self, request, sociallogin):
-        # Automatically create the user if it doesn't exist
-    
-
-        if not sociallogin.is_existing:
+       if not sociallogin.is_existing:
             user = User.objects.filter(email=sociallogin.account.extra_data['email']).first()
             if user:
-                sociallogin.connect(request, user)
-
+                messages.error(request, "Email đã tồn tại. Vui lòng đăng nhập bằng tên người dùng và mật khẩu của bạn.")
+                raise ImmediateHttpResponse(redirect('account_login'))
+            
             user = sociallogin.user
             user.email = sociallogin.account.extra_data['email']
             user.first_name = sociallogin.account.extra_data.get('given_name', '')
             user.last_name = sociallogin.account.extra_data.get('family_name', '')
-            user.username = sociallogin.account.extra_data['email']
+            # user.username = sociallogin.account.extra_data['email']
             user.avatar_url = sociallogin.account.extra_data.get('picture', '')
             user.save()
             sociallogin.connect(request, user)
