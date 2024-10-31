51c52a57ff795557af3cfc31c8d976f1759e7e1a
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2024-10-31 20:24:05 +0700

    fix login

--- store/adapter.py
+++ store/adapter.py
@@ -15,9 +15,10 @@ class NoSignupSocialAccountAdapter(DefaultSocialAccountAdapter):
         user.email = sociallogin.account.extra_data['email']
         user.first_name = sociallogin.account.extra_data.get('given_name', '')
         user.last_name = sociallogin.account.extra_data.get('family_name', '')
-        # Save avatar if available
-        
-       
-        user.avatar_url = 'https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png'
+        user.username = sociallogin.account.extra_data['email']
+        if sociallogin.account.provider == 'google':
+            user.avatar_url = sociallogin.account.extra_data.get('picture', '')
+        else:
+            user.avatar_url = 'https://png.pngtree.com/png-vector/20220709/ourmid/pngtree-businessman-user-avatar-wearing-suit-with-red-tie-png-image_5809521.png'
         user.save()
         return user
