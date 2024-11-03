cb286373690697ab1cfc6c2c792b6770e106c36c
duyan77 <fklt0961@gmail.com>
2024-11-03 11:57:29 +0700

    Revert "completing"
    This reverts commit b933f1cd

--- cart/views.py
+++ cart/views.py
@@ -1,26 +1,22 @@
-from http.client import responses
-
 from django.http import JsonResponse
 from django.shortcuts import render
 from .cart import Cart
 from store.models import Product
 from django.shortcuts import get_object_or_404
 
-
 def cart_summary(request):
-	return render(request, 'cart/cart-summary.html')
+	return render(request, 'cart/cart-summary.html', )
 
 
-def cart_add(request):
+def cart_add(request, product_id):
 	cart = Cart(request)
-	if request.POST.get('action') == 'post':
+	if request.POST.get('action') == 'POST':
 		product_id = int(request.POST.get('product_id'))
 		product_quantity = int(request.POST.get('product_quantity'))
 		product = get_object_or_404(Product, id=product_id)
 
 		cart.add(product=product, product_qty=product_quantity)
-		response = JsonResponse({'qty': product_quantity})
-		return response
+		return JsonResponse({'qty': cart.__len__()})
 
 
 def cart_delete(request, product_id):
