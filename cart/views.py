b933f1cd89c3823e4a6b82894bc58b09acfa693a
duyan77 <fklt0961@gmail.com>
2024-11-03 10:59:05 +0700

    completing

--- cart/views.py
+++ cart/views.py
@@ -1,22 +1,26 @@
+from http.client import responses
+
 from django.http import JsonResponse
 from django.shortcuts import render
 from .cart import Cart
 from store.models import Product
 from django.shortcuts import get_object_or_404
 
+
 def cart_summary(request):
-	return render(request, 'cart/cart-summary.html', )
+	return render(request, 'cart/cart-summary.html')
 
 
-def cart_add(request, product_id):
+def cart_add(request):
 	cart = Cart(request)
-	if request.POST.get('action') == 'POST':
+	if request.POST.get('action') == 'post':
 		product_id = int(request.POST.get('product_id'))
 		product_quantity = int(request.POST.get('product_quantity'))
 		product = get_object_or_404(Product, id=product_id)
 
 		cart.add(product=product, product_qty=product_quantity)
-		return JsonResponse({'qty': cart.__len__()})
+		response = JsonResponse({'qty': product_quantity})
+		return response
 
 
 def cart_delete(request, product_id):
