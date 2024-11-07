55d8a523cf150cf53404b93c6e1b03f55f6696ab
Nguyen Thanh Trieu <trieukon1011@gmail.com>
2024-11-07 19:19:08 +0700

    A

--- store/models.py
+++ store/models.py
@@ -18,6 +18,7 @@ class User(AbstractUser):
 
 
 class Category(models.Model):
+	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=255, db_index=True)
 	slug = models.SlugField(max_length=255, unique=True)
 
@@ -31,25 +32,119 @@ class Category(models.Model):
 		return reverse('list-category', args=[self.slug])
 
 
+class Brand(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=255)
+	address = models.TextField(blank=True)
+	slug = models.SlugField(max_length=255, unique=True)
+
+	class Meta:
+		verbose_name_plural = 'brands'
+
+	def __str__(self):
+		return self.name
+
+	def get_absolute_url(self):
+		return reverse('list-brand', args=[self.slug])
+
 class Product(models.Model):
-	# FK
-	category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE,
-								 null=True)  # null=True is added to allow for products without a category
+	id = models.AutoField(primary_key=True)
+	category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE,null=True)  
+	brand = models.ForeignKey(Brand, related_name='products', on_delete=models.CASCADE,null=True)
 	title = models.CharField(max_length=255)
-	brand = models.CharField(max_length=255, default='un-branded')
 	description = models.TextField(blank=True)
 	slug = models.SlugField(max_length=255, unique=True)
-	price = models.DecimalField(max_digits=10, decimal_places=0)
-	image = models.ImageField(upload_to='images/')
-
+	
 	class Meta:
 		verbose_name_plural = 'products'
 
 	def __str__(self):
 		return self.title
 
-	def formatted_price(self):
-		return f"{self.price:,.0f}₫"
-
 	def get_absolute_url(self):
 		return reverse('product-info', args=[self.slug])
+
+class ProductInfo(models.Model):
+	id = models.OneToOneField(Product, primary_key=True, on_delete=models.CASCADE)
+	cpu = models.CharField(max_length=255)
+	camera = models.CharField(max_length=255)
+	battery = models.CharField(max_length=255)
+	screen = models.CharField(max_length=255)
+	note = models.TextField(blank=True)
+
+	def __str__(self):
+		return self.product.title
+	
+class ProductDetail(models.Model):
+	id = models.AutoField(primary_key=True)
+	product_id = models.ForeignKey(Product, related_name='details', on_delete=models.CASCADE)
+	color = models.CharField(max_length=255)
+	ram = models.CharField(max_length=255)
+	storage = models.CharField(max_length=255)
+	price = models.DecimalField(max_digits=60, decimal_places=2)
+	quantity = models.IntegerField()
+
+	def __str__(self):
+		return self.product.title
+
+class ProductImage(models.Model):
+	id = models.AutoField(primary_key=True)
+	product_id = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+	image = models.ImageField(upload_to='images/')
+	product_detail_id = models.ForeignKey(ProductDetail, related_name='images', on_delete=models.CASCADE, null=True)
+
+	def __str__(self):
+		return self.product.title
+	
+
+	
+class Review(models.Model):
+	id = models.AutoField(primary_key=True)
+	product_id = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+	user_id = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
+	rating = models.IntegerField()
+	comment = models.TextField(blank=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.product.title
+	
+class Cart(models.Model):
+	id = models.AutoField(primary_key=True)
+	user_id = models.ForeignKey(User, related_name='carts', on_delete=models.CASCADE)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.user.username
+	
+class CartItem(models.Model):
+	id = models.AutoField(primary_key=True)
+	cart_id = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+	product_detail_id = models.ForeignKey(ProductDetail, related_name='cart_items', on_delete=models.CASCADE)
+	quantity = models.IntegerField()
+
+	
+
+	def __str__(self):
+		return self.product.title
+	
+class Order(models.Model):
+	id = models.AutoField(primary_key=True)
+	user_id = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=255)
+	total = models.DecimalField(max_digits=60, decimal_places=2)
+	shiper = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.user.username
+	
+class OrderItem(models.Model):
+	id = models.AutoField(primary_key=True)
+	order_id = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+	product_detail_id = models.ForeignKey(ProductDetail, related_name='order_items', on_delete=models.CASCADE)
+	quantity = models.IntegerField()
+
+	def __str__(self):
+		return self.product.title
